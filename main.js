d2747416c69a9c23f266e1942e3fbcca3b7e56f0
fritzcrone <fritz.crone@gmail.com>
2025-05-08 16:14:54 +0200

    new Date

--- main.js
+++ main.js
@@ -51,6 +51,8 @@ async function loadStations(url) {
             });
         },
         onEachFeature: function (feature, layer) {
+            let pointInTime = new Date(feature.properties.date);
+            //console.log(pointInTime)
             //console.log(feature);
             layer.bindPopup(`
                 <h4>${feature.properties.name} (${feature.geometry.coordinates[2]}m)</h4>
@@ -60,7 +62,7 @@ async function loadStations(url) {
                 <li>Windgeschwindigkeit (km/h): ${feature.properties.WG || "-"}</li>
                 <li>Schneehöhe (cm): ${feature.properties.HS || "-"}</li>
                 </ul>
-                <span></span>
+                <span>${feature.properties.date}</span>
         `);
         }
     }).addTo(overlays.stations);
